11aa3efe236dd092a967ed6efb88f9a850c57ecb
jacques mouette <j.mouette@gmail.com>
2016-05-18 17:11:57 -0300

    Adicionando hub simples de SignalR; removendo informações inúteis Startup.Auth.cs

---
+++ TheLearningMaze-API/Hubs/EventoHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TheLearningMaze_API.Hubs
+{
+    public class EventoHub : Hub
+    {
+        public void LancarPergunta(string pergunta)
+        {
+            Clients.Others.lancarPergunta(pergunta);
+        }
+
+        public void ResponderPergunta()
+        {
+            Clients.Others.responderPergunta();
+        }
+    }
+}
