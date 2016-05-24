4ca055c66ae68a7a926b7dd1a55e978a5d246fa9
jacques mouette <j.mouette@gmail.com>
2016-05-24 16:46:46 -0300

    descomentando authfilter :)

--- TheLearningMaze-API/Controllers/EventosController.cs
+++ TheLearningMaze-API/Controllers/EventosController.cs
@@ -13,7 +13,7 @@ using TheLearningMaze_API.Models;
 
 namespace TheLearningMaze_API.Controllers
 {
-    //[ProfAuthFilter]
+    [ProfAuthFilter]
     public class EventosController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
