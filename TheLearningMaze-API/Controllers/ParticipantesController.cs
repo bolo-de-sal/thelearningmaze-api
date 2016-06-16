071fc1421da5dcb149aa0cba1a1158ea36e2a417
jacques mouette <j.mouette@gmail.com>
2016-06-16 11:22:36 -0300

    ajustes nas validacoes de retorno vazios

--- TheLearningMaze-API/Controllers/ParticipantesController.cs
+++ TheLearningMaze-API/Controllers/ParticipantesController.cs
@@ -26,7 +26,7 @@ namespace TheLearningMaze_API.Controllers
                                     .Where(q => q.codEvento == id && q.codStatus == "E")
                                     .Select(q => q.codQuestao)
                                     .FirstOrDefault();
-            if (codQuestaoAtual == null || codQuestaoAtual == 0) return Content(HttpStatusCode.NotFound, new { message = "Não há questão em execução neste evento!" });
+            if (codQuestaoAtual == 0) return Content(HttpStatusCode.NotFound, new { message = "Não há questão em execução neste evento!" });
 
             Questao questao = db.Questaos.FirstOrDefault(q => q.codQuestao == codQuestaoAtual);
 
@@ -41,7 +41,7 @@ namespace TheLearningMaze_API.Controllers
                                     .Where(q => q.codEvento == id && q.codStatus == "E")
                                     .Select(q => q.codQuestao)
                                     .FirstOrDefault();
-            if (codQuestaoAtual == null && codQuestaoAtual == 0) return Content(HttpStatusCode.NotFound, new { message = "Não há questão em execução neste evento!" });
+            if (codQuestaoAtual == 0) return Content(HttpStatusCode.NotFound, new { message = "Não há questão em execução neste evento!" });
 
             string tipoQuestao = db.Questaos
                         .Where(q => q.codQuestao == codQuestaoAtual)
@@ -63,7 +63,7 @@ namespace TheLearningMaze_API.Controllers
         public IHttpActionResult RegistrarPerguntas(Evento ev, Questao[] q)
         {
             int? e = db.Eventos.Where(w => w.codEvento == ev.codEvento).Select(w => w.codEvento).FirstOrDefault();
-            if (e == null && e == 0) return Content(HttpStatusCode.BadRequest, new { message = "Não foi enviado evento válido!" });
+            if (e == 0) return Content(HttpStatusCode.BadRequest, new { message = "Não foi enviado evento válido!" });
             if (q == null) return Content(HttpStatusCode.BadRequest, new { message = "Não foram enviadas questões!" });
 
             foreach (Questao questao in q)
